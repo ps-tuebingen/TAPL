892eebb2aed1cb98c309d31f1b9dc47d28db0f26
MarcoTz <marco.tzschentke@gmail.com>
2024-11-29 10:16:55 +0100

    added main parsing and evaluating cli arguments

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("hello")
+use std::env;
+use untyped_lambda::{
+    eval::{eval, EvalOrder},
+    parse::parse,
+};
+
+fn main() -> Result<(), String> {
+    let mut args = env::args();
+    //remove file name
+    args.next();
+    let mut arg_str = args.collect::<Vec<String>>().join(" ");
+    let parsed = parse(&mut arg_str).map_err(|err| err.to_string())?;
+    println!("Parsed term:\n{parsed}");
+    let evaled = eval(parsed, EvalOrder::CBV);
+    println!("Evaled term:\n{evaled}");
+    Ok(())
 }
