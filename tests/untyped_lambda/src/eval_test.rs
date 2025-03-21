fc8e62e9963e7404b95b2f0d2dc7270a07abe39e
MarcoTz <marco.tzschentke@gmail.com>
2025-03-21 16:07:38 +0100

    refactored untyped lambda tests

---
+++ tests/untyped_lambda/src/eval_test.rs
@@ -0,0 +1,37 @@
+use test_common::testsuite::{Test, TestResult};
+use untyped_lambda::{
+    eval::{eval, EvalOrder},
+    parse::parse,
+};
+
+pub struct EvalTest {
+    source_name: String,
+    source_contents: String,
+    expected: String,
+}
+
+impl EvalTest {
+    pub fn new(source_name: &str, source: &str, expected: &str) -> EvalTest {
+        EvalTest {
+            source_name: source_name.to_owned(),
+            source_contents: source.to_owned(),
+            expected: expected.to_owned(),
+        }
+    }
+}
+
+impl Test for EvalTest {
+    fn name(&self) -> String {
+        format!("Evaluate {}", self.source_name)
+    }
+
+    fn run(&self) -> TestResult {
+        let mut src = self.source_contents.clone();
+        let parsed = match parse(&mut src) {
+            Ok(p) => p,
+            Err(err) => return TestResult::from_err(err),
+        };
+        let evaled = eval(parsed, EvalOrder::CBV);
+        TestResult::from_eq(&evaled, &self.expected)
+    }
+}
---
+++ tests/untyped_lambda/src/parse_test.rs
@@ -0,0 +1,30 @@
+use test_common::testsuite::{Test, TestResult};
+use untyped_lambda::parse::parse;
+
+pub struct ParseTest {
+    source_name: String,
+    source_contents: String,
+}
+
+impl ParseTest {
+    pub fn new(source_name: &str, source: &str) -> ParseTest {
+        ParseTest {
+            source_name: source_name.to_owned(),
+            source_contents: source.to_owned(),
+        }
+    }
+}
+
+impl Test for ParseTest {
+    fn name(&self) -> String {
+        format!("Parse {}", self.source_name)
+    }
+
+    fn run(&self) -> TestResult {
+        let mut src = self.source_contents.clone();
+        match parse(&mut src) {
+            Ok(_) => TestResult::Success,
+            Err(err) => return TestResult::from_err(err),
+        }
+    }
+}
