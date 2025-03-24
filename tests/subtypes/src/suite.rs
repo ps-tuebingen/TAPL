46cd940f857f926bbb347a5b511a99aa8bb5d3b4
MarcoTz <marco.tzschentke@gmail.com>
2025-03-24 16:03:42 +0100

    added parse and reparse tests for subtypes

--- tests/subtypes/src/suite.rs
+++ tests/subtypes/src/suite.rs
@@ -1,3 +1,4 @@
+use super::{ParseTest, ReparseTest};
 use std::path::PathBuf;
 use test_common::{
     errors::Error,
@@ -28,7 +29,12 @@ impl TestSuite for SubtypesTests {
     fn load(&self) -> Result<Vec<Box<dyn Test>>, Error> {
         let contents: Vec<TestContents<SubtypesConf>> = load_dir(&self.source_path, "sub")?;
         let mut tests = vec![];
-        for tst in contents {}
+        for tst in contents {
+            let parse_test = ParseTest::new(&tst.source_name, &tst.source_contents);
+            tests.push(Box::new(parse_test) as Box<dyn Test>);
+            let reparse_test = ReparseTest::new(&tst.source_name, &tst.source_contents);
+            tests.push(Box::new(reparse_test) as Box<dyn Test>);
+        }
         Ok(tests)
     }
 }
