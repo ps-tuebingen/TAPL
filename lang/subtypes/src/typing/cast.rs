74554f1d89329b963116e93927cc2ef8ef32c9cf
MarcoTz <marco.tzschentke@gmail.com>
2025-03-24 21:48:26 +0100

    fixed some examples

--- lang/subtypes/src/typing/cast.rs
+++ lang/subtypes/src/typing/cast.rs
@@ -1,13 +1,9 @@
-use super::{errors::Error, is_subtype, Typecheck, TypingContext};
+use super::{errors::Error, Typecheck, TypingContext};
 use crate::{syntax::Cast, types::Type};
 
 impl Typecheck for Cast {
     fn check(&self, env: &mut TypingContext) -> Result<Type, Error> {
-        let inner = self.term.check(env)?;
-        if is_subtype(&inner, &self.ty) {
-            Ok(self.ty.clone())
-        } else {
-            Err(Error::TypeMismatch(inner, self.ty.clone()))
-        }
+        self.term.check(env)?;
+        Ok(self.ty.clone())
     }
 }
