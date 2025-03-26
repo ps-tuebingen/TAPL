34b1fa1386d04df3ff48438c154a8a91baa009d1
MarcoTz <marco.tzschentke@gmail.com>
2025-03-26 18:07:30 +0100

    fixed isnil

--- examples/systemf/isnil/isnil.f
+++ examples/systemf/isnil/isnil.f
@@ -1,2 +1,2 @@
 \X.\l:(forall R.(X->R->R)->R->R).
-  ((l[Bool])(\hd:X.\tl:Bool.(\X.\t:X.\f:X.f)))(\X.\t:X.\f:X.t)
+  ((l[forall X.((X -> (X -> X)))])(\hd:X.\tl:(forall X.((X -> (X -> X)))).(\X.\t:X.\f:X.f)))(\X.\t:X.\f:X.t)
