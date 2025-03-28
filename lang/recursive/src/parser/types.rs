2ea9c0e8460f313170ac846355987d203785859f
MarcoTz <marco.tzschentke@gmail.com>
2025-03-28 18:52:13 +0100

    fixed pair types

--- lang/recursive/src/parser/types.rs
+++ lang/recursive/src/parser/types.rs
@@ -28,6 +28,7 @@ fn pair_to_prim_type(p: Pair<'_, Rule>) -> Result<Type, Error> {
     match p.as_rule() {
         Rule::const_type => str_to_type(p.as_str()),
         Rule::mu_type => pair_to_mu_type(p),
+        Rule::pair_type => pair_to_pair_type(p),
         Rule::variant_type => pair_to_variant_type(p),
         Rule::record_type => pair_to_record_type(p),
         Rule::paren_type => {
@@ -42,7 +43,6 @@ fn pair_to_prim_type(p: Pair<'_, Rule>) -> Result<Type, Error> {
 fn pair_to_leftrec_type(p: Pair<'_, Rule>, ty: Type) -> Result<Type, Error> {
     match p.as_rule() {
         Rule::fun_type => pair_to_fun_type(p, ty),
-        Rule::pair_type => pair_to_pair_type(p, ty),
         r => Err(Error::unexpected(r, "Left Recursive Term")),
     }
 }
@@ -102,8 +102,11 @@ fn pair_to_fun_type(p: Pair<'_, Rule>, ty: Type) -> Result<Type, Error> {
     })
 }
 
-fn pair_to_pair_type(p: Pair<'_, Rule>, ty: Type) -> Result<Type, Error> {
-    let inner = pair_to_n_inner(p, vec!["Pair Second Type"])?.remove(0);
-    let snd_ty = pair_to_type(inner)?;
-    Ok(Type::Pair(Box::new(ty), Box::new(snd_ty)))
+fn pair_to_pair_type(p: Pair<'_, Rule>) -> Result<Type, Error> {
+    let mut inner = pair_to_n_inner(p, vec!["Pair First Type", "Pair Second Type"])?;
+    let fst_rule = inner.remove(0);
+    let fst = pair_to_type(fst_rule)?;
+    let snd_rule = inner.remove(0);
+    let snd = pair_to_type(snd_rule)?;
+    Ok(Type::Pair(Box::new(fst), Box::new(snd)))
 }
