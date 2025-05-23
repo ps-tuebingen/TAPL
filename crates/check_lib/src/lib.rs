898aeefba1158e67420eb21e2417fb729a32018a
MarcoTz <marco.tzschentke@gmail.com>
2025-05-23 10:17:33 +0200

    fixed directory structure

---
+++ crates/check_lib/src/lib.rs
@@ -0,0 +1,59 @@
+use crate::{
+    errors::{Error, ErrorKind, ErrorLocation},
+    kinds::Kind,
+    language::LanguageType,
+    types::Type,
+};
+
+pub mod env;
+pub use env::CheckEnvironment;
+
+pub trait Typecheck {
+    type Type: LanguageType;
+    type Env: CheckEnvironment<Type = Self::Type>;
+
+    fn check_start(&self) -> Result<Self::Type, Error> {
+        self.check(&mut Self::Env::default())
+    }
+
+    fn check(&self, env: &mut Self::Env) -> Result<Self::Type, Error>;
+}
+
+pub trait Subtypecheck<Ty>
+where
+    Self: Type,
+    Ty: LanguageType,
+{
+    type Env: CheckEnvironment<Type = Ty>;
+
+    fn check_subtype(&self, sup: &Ty, env: &mut Self::Env) -> Result<(), Error>;
+}
+
+pub trait Kindcheck<Ty>
+where
+    Ty: LanguageType,
+{
+    type Env: CheckEnvironment<Type = Ty>;
+    fn check_kind(&self, env: &mut Self::Env) -> Result<Kind, Error>;
+}
+
+pub fn to_check_err(knd: ErrorKind) -> Error {
+    Error {
+        kind: knd,
+        loc: ErrorLocation::Check,
+    }
+}
+
+pub fn to_subty_err(knd: ErrorKind) -> Error {
+    Error {
+        kind: knd,
+        loc: ErrorLocation::Subtyping,
+    }
+}
+
+pub fn to_kind_err(knd: ErrorKind) -> Error {
+    Error {
+        kind: knd,
+        loc: ErrorLocation::Kind,
+    }
+}
---
+++ crates/language_core_lib/src/lib.rs
@@ -0,0 +1,21 @@
+mod terms;
+mod types;
+pub mod untyped;
+mod values;
+
+pub use terms::LanguageTerm;
+pub use types::LanguageType;
+pub use values::LanguageValue;
+
+pub trait Language {
+    type Term: LanguageTerm<Type = Self::Type, Value = Self::Value>
+        + Typecheck<Env = Self::CheckEnv, Type = Self::Type>
+        + Eval<Value = Self::Value, Env = Self::EvalEnv>;
+    type Type: LanguageType
+        + Subtypecheck<Self::Type, Env = Self::CheckEnv>
+        + Normalize<Self::Type, Env = Self::CheckEnv>;
+    type Value: LanguageValue<Term = Self::Term>;
+
+    type CheckEnv: CheckEnvironment<Type = Self::Type>;
+    type EvalEnv: EvalEnvironment<Self::Value>;
+}
