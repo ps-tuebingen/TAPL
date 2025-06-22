1a4f2ad9abbd88d735c89c6cf51f4c8fbe845a78
MarcoTz <marco.tzschentke@gmail.com>
2025-06-22 14:40:01 +0200

    added store

--- crates/syntax_lib/src/lib.rs
+++ crates/syntax_lib/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod env;
 pub mod kinds;
 pub mod program;
+pub mod store;
 pub mod subst;
 pub mod terms;
 pub mod types;
---
+++ crates/syntax_lib/src/store.rs
@@ -0,0 +1,49 @@
+use crate::{values::Value, Location};
+use common::errors::UndefinedLocation;
+use std::collections::HashMap;
+
+pub struct Store<V>
+where
+    V: Value,
+{
+    locs: HashMap<Location, V>,
+}
+
+impl<V> Store<V>
+where
+    V: Value,
+{
+    pub fn new() -> Store<V> {
+        Store {
+            locs: HashMap::new(),
+        }
+    }
+
+    pub fn fresh_location(&self) -> Location {
+        let mut new_loc = 0;
+        while self.locs.contains_key(&new_loc) {
+            new_loc += 1;
+        }
+        new_loc
+    }
+
+    pub fn save_location(&mut self, loc: Location, v: V) {
+        self.locs.insert(loc, v);
+    }
+
+    pub fn get_location(&self, loc: Location) -> Result<V, UndefinedLocation> {
+        self.locs
+            .get(&loc)
+            .cloned()
+            .ok_or(UndefinedLocation::new(loc))
+    }
+}
+
+impl<V> Default for Store<V>
+where
+    V: Value,
+{
+    fn default() -> Store<V> {
+        Store::new()
+    }
+}
