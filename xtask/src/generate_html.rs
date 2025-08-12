ddbf60e6df9e6be2bedbf33ed5161f3971648054
MarcoTz <marco.tzschentke@gmail.com>
2025-08-12 17:29:45 +0200

    added separate css files

--- xtask/src/generate_html.rs
+++ xtask/src/generate_html.rs
@@ -1,5 +1,6 @@
 use errors::{DirAccess, FileAccess, build_error::BuildError};
 use std::{
+    ffi::OsStr,
     fs::{File, read_dir, read_to_string, remove_file},
     io::Write,
     path::PathBuf,
@@ -8,12 +9,49 @@ use std::{
 const TEMPLATES_PATH: &str = "html/templates";
 const OUT_PATH: &str = "html";
 const TEMPLATE_FILE: &str = "html/template.html";
-const TEMPLATE_PLACEHOLDER: &str = "{{ CONTENT }}";
+
+const TEMPLATE_PLACEHOLDER: &str = "{{- CONTENT -}}";
+const PAGE_NAME_PLACEHOLDER: &str = "{{- PAGE_NAME -}}";
+
+fn load_template() -> Result<String, BuildError> {
+    read_to_string(TEMPLATE_FILE).map_err(|err| FileAccess::new("Read html template", err).into())
+}
+
+fn fill_template(path: &PathBuf, template_contents: &str) -> Result<String, BuildError> {
+    let file_stem = path.file_stem().ok_or(FileAccess::new(
+        "Read template file stem",
+        "Could not read template file stem",
+    ))?;
+
+    let file_stem_str = file_stem.to_str().ok_or(FileAccess::new(
+        "Read template file stem as str",
+        "Could not read template file stem as str",
+    ))?;
+
+    let template_fill =
+        read_to_string(&path).map_err(|err| FileAccess::new("Read template contents", err))?;
+
+    Ok(template_contents
+        .to_owned()
+        .replace(TEMPLATE_PLACEHOLDER, &template_fill)
+        .replace(PAGE_NAME_PLACEHOLDER, file_stem_str))
+}
+
+fn write_template(template_file_name: &OsStr, out_contents: &str) -> Result<(), BuildError> {
+    let out_path = PathBuf::from(OUT_PATH).join(template_file_name);
+
+    if out_path.exists() {
+        remove_file(&out_path).map_err(|err| FileAccess::new("Delete old html file", err))?;
+    }
+    let mut out_file =
+        File::create(out_path).map_err(|err| FileAccess::new("Create new html file", err))?;
+    out_file
+        .write_all(out_contents.as_bytes())
+        .map_err(|err| FileAccess::new("Write html contents", err).into())
+}
 
 pub fn generate_html() -> Result<(), BuildError> {
-    let template_contents =
-        read_to_string(TEMPLATE_FILE).map_err(|err| FileAccess::new("Read html template", err))?;
-
+    let template_contents = load_template()?;
     let templates_dir =
         read_dir(TEMPLATES_PATH).map_err(|err| DirAccess::new("Read html templates dir", err))?;
     for template_file in templates_dir {
@@ -24,22 +62,9 @@ pub fn generate_html() -> Result<(), BuildError> {
             "Read template file name",
             "Could not read template file name",
         ))?;
-        let template_fill = read_to_string(&template_path)
-            .map_err(|err| FileAccess::new("Read template contents", err))?;
 
-        let out_path = PathBuf::from(OUT_PATH).join(template_file_name);
-        let out_contents = template_contents
-            .clone()
-            .replace(TEMPLATE_PLACEHOLDER, &template_fill);
-
-        if out_path.exists() {
-            remove_file(&out_path).map_err(|err| FileAccess::new("Delete old html file", err))?;
-        }
-        let mut out_file =
-            File::create(out_path).map_err(|err| FileAccess::new("Create new html file", err))?;
-        out_file
-            .write_all(out_contents.as_bytes())
-            .map_err(|err| FileAccess::new("Write html contents", err))?;
+        let out_contents = fill_template(&template_path, &template_contents)?;
+        write_template(template_file_name, &out_contents)?;
     }
     Ok(())
 }
