5de982d128824aa686cf92e68858018ed0000fa4
MarcoTz <marco.tzschentke@gmail.com>
2026-01-05 14:54:15 +0100

    fixed generating constraints for variantcaste

--- lib/inference/src/generate_constraints/terms/iszero.rs
+++ lib/inference/src/generate_constraints/terms/iszero.rs
@@ -1,19 +1,23 @@
 use super::{EqualityConstraint, GenState, GenerateConstraints};
-use syntax::{language::Language, terms::IsZero, types::Nat};
+use syntax::{
+    language::Language,
+    terms::IsZero,
+    types::{Bool, Nat},
+};
 
 impl<Lang> GenerateConstraints for IsZero<Lang>
 where
     Lang: Language,
     Lang::Term: GenerateConstraints<Lang = Lang, Target = Lang::Type>,
     Nat<Lang>: Into<Lang::Type>,
+    Bool<Lang>: Into<Lang::Type>,
 {
     type Lang = Lang;
     type Target = Lang::Type;
 
     fn generate_constraints(&self, state: &mut GenState<Lang>) -> Self::Target {
         let term_ty = self.term.generate_constraints(state);
-        let nat = Nat::new(self.span);
-        state.add_constraint(EqualityConstraint::new(term_ty, nat.clone()));
-        nat.into()
+        state.add_constraint(EqualityConstraint::new(term_ty, Nat::new(self.span)));
+        Bool::new(self.span).into()
     }
 }
